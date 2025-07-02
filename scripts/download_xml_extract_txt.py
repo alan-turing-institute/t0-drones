ec6475eab2e425d30570ad3a8fa5c37134819df4
fedenanni <nanni.federico@gmail.com>
2025-07-02 17:22:20 +0800

    Update extract script

--- scripts/download_xml_extract_txt.py
+++ scripts/download_xml_extract_txt.py
@@ -1,18 +1,37 @@
-from utils import download_xml, read_xml_content, extract_text_from_xml
 import os
+import sys
+from utils import download_xml, read_xml_content, extract_text_from_xml
 
-# URL of the plain view of Annex IX
-URL = "https://www.legislation.gov.uk/eur/2018/1139/annex/IX?view=xml"
 
-if not os.path.exists("data"):
-    os.makedirs("data")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py https://www.legislation.gov.uk/path/to/xml")
+        sys.exit(1)
 
-OUTPUT_FILE = "data/annex_ix_cleaned.txt"
+    url = sys.argv[1]
 
-download_xml(URL, "data/annex_ix.xml")
-xml_content = read_xml_content("annex_ix.xml")
-text = extract_text_from_xml(xml_content)
-# Save the cleaned text
-with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-    f.write(text)
-print(f"Saved cleaned text to {OUTPUT_FILE}")
+    if not os.path.exists("data"):
+        os.makedirs("data")
+
+    # Create a filename based on the URL
+    filename = (
+        url.split("https://www.legislation.gov.uk/")[1].split("?")[0].replace("/", "_")
+    )
+
+    xml_path = f"data/{filename}.xml"
+    txt_path = f"data/{filename}.txt"
+
+    # Download and process XML
+    download_xml(url, xml_path)
+    xml_content = read_xml_content(xml_path)
+    text = extract_text_from_xml(xml_content)
+
+    # Save the cleaned text
+    with open(txt_path, "w", encoding="utf-8") as f:
+        f.write(text)
+
+    print(f"Saved cleaned text to {txt_path}")
+
+
+if __name__ == "__main__":
+    main()
